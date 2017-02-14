c81e9e740f6cacd69f0a8d3d9d55c45e0e04069b
Leo.Scream <mod.satyr@gmail.com>
2017-02-14 11:09:50 +0300

    add task 3

---
+++ src/main/java/com/github/leo_scream/java_se_course/task03/Solution.java
@@ -0,0 +1,31 @@
+package com.github.leo_scream.java_se_course.task03;
+
+/**
+ * @author Denis Verkhoturov, mod.satyr@gmail.com
+ */
+public class Solution
+{
+    private final double step;
+
+    public Solution(double h)
+    {
+        this.step = Math.abs(h);
+    }
+
+    protected double calculate(double x)
+    {
+        return Math.tan(2 * x) - 3;
+    }
+
+    public void onRange(double a, double b)
+    {
+        final double min = a < b ? a : b;
+        final double max = a > b ? a : b;
+
+        System.out.println("|      x     |     F(x)   |");
+        System.out.println("|------------|------------|");
+        for (double x = min; x <= max; x += step) {
+            System.out.format("| %+10.3f | %+10.3f |\n", x, calculate(x));
+        }
+    }
+}
---
+++ src/test/java/com/github/leo_scream/java_se_course/task03/SolutionTest.java
@@ -0,0 +1,16 @@
+package com.github.leo_scream.java_se_course.task03;
+
+import org.junit.Test;
+
+/**
+ * @author Denis Verkhoturov, mod.satyr@gmail.com
+ */
+public class SolutionTest
+{
+    @Test
+    public void onRange() throws Exception
+    {
+        Solution solution = new Solution(0.1);
+        solution.onRange(0, 1);
+    }
+}
