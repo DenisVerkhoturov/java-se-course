f989bda4af4fb163859ba57d41dbd61f21a29da5
Leo.Scream <mod.satyr@gmail.com>
2017-02-17 22:35:52 +0300

    Notebook task and test, README updated

--- src/main/java/com/github/leo_scream/java_se_course/task06/Notebook.java
+++ src/main/java/com/github/leo_scream/java_se_course/task06/Notebook.java
@@ -16,13 +16,44 @@ public class Notebook
         this.size = 0;
     }
 
+    /**
+     * @return Number of elements contained in notebook
+     */
     public int getSize()
     {
         return size;
     }
 
     /**
-     * @param capacity Required capacity
+     * @param i
+     * 		Check if {@code i} in range of size
+     *
+     * @throws IndexOutOfBoundsException
+     * 		If {@code i} negative or bigger that {@code size}
+     */
+    private void rangeCheck(int i)
+    {
+        if (i < 0 || i >= size)
+            throw new IndexOutOfBoundsException();
+    }
+
+    /**
+     * @param note
+     * 		Check if {@code note} is null
+     *
+     * @throws IllegalArgumentException
+     * 		If {@code note} is null
+     */
+    private void nullCheck(Note note)
+    {
+        if (note == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * @param capacity
+     * 		Required capacity
      */
     private void ensureCapacity(int capacity)
     {
@@ -32,17 +63,105 @@ public class Notebook
         }
     }
 
-    public void add(Note note) {
+    private void smartRemove(int i)
+    {
+        if (size > 1) {
+            System.arraycopy(notes, i + 1, notes, i, size - 1);
+        } else {
+            notes = new Note[0];
+        }
+        size -= 1;
+    }
+
+    /**
+     * Adds {@code note} to notebook.
+     *
+     * @param note
+     * 		Note to be added to notebook
+     *
+     * @return Index of newly added {@code note}
+     *
+     * @throws IllegalArgumentException
+     * 		If {@code note} is {@code null}
+     */
+    public int add(Note note)
+    {
+        nullCheck(note);
         ensureCapacity(size + 1);
         notes[size] = note;
         size += 1;
+        return size - 1;
     }
 
-    public void remove(int i) {}
+    /**
+     * Removes note under index {@code i}.
+     *
+     * @param i
+     * 		Index of note to delete
+     *
+     * @throws IndexOutOfBoundsException
+     * 		If {@code i} negative or bigger that {@code size}
+     */
+    public void remove(int i)
+    {
+        rangeCheck(i);
+        smartRemove(i);
+    }
 
-    public void remove(Note note) {}
+    /**
+     * Removes {@code note} from notebook.
+     *
+     * @param note
+     * 		Note which will be removed
+     *
+     * @return Index of removed note or {@code -1} if note is not found
+     *
+     * @throws IllegalArgumentException
+     * 		If {@code note} is {@code null}
+     */
+    public int remove(Note note)
+    {
+        nullCheck(note);
 
-    public void update(int i) {}
+        for (int i = 0; i < size; i++) {
+            if (notes[i].equals(note)) {
+                smartRemove(i);
+                return i;
+            }
+        }
+        return -1;
+    }
 
-    public void get(int i) {}
+    /**
+     * Update note under index {@code i}.
+     *
+     * @param i
+     * 		Index of {@code note} which will be updated
+     *
+     * @throws IndexOutOfBoundsException
+     * 		If {@code i} negative or bigger that {@code size}
+     * @throws IllegalArgumentException
+     * 		If {@code note} is {@code null}
+     */
+    public void update(int i, Note note)
+    {
+        nullCheck(note);
+        rangeCheck(i);
+        notes[i] = note;
+    }
+
+    /**
+     * @param i
+     * 		Index of note to get
+     *
+     * @return Note under index {@code i}
+     *
+     * @throws IndexOutOfBoundsException
+     * 		If {@code i} negative or bigger that {@code size}
+     */
+    public Note get(int i)
+    {
+        rangeCheck(i);
+        return notes[i];
+    }
 }
