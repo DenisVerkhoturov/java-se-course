26539bc40548b96bdda898be0ca2f73995e86f23
Leo.Scream <mod.satyr@gmail.com>
2017-03-10 20:49:32 +0300

    Remove unused methods from ProgrammingLanguage

--- unit_04/src/main/java/com/github/leo_scream/java_se_course/unit_04/shared/ProgrammingLanguage.java
+++ unit_04/src/main/java/com/github/leo_scream/java_se_course/unit_04/shared/ProgrammingLanguage.java
@@ -1,34 +1,26 @@
 package com.github.leo_scream.java_se_course.unit_04.shared;
 
 import java.util.Set;
-import java.util.stream.Stream;
 
 /**
  * @author Denis Verkhoturov, mod.satyr@gmail.com
  */
-public class ProgrammingLanguage
-{
+public class ProgrammingLanguage {
+
     private final String name;
     private final Set<String> keywords;
 
-    public ProgrammingLanguage(String name, Set<String> keywords)
-    {
+    public ProgrammingLanguage(String name, Set<String> keywords) {
         this.name = name;
         this.keywords = keywords;
     }
 
-    public boolean isKeyword(String word)
+    public String getName()
     {
+        return name;
+    }
+
+    public boolean isKeyword(String word) {
         return keywords.contains(word);
-    }
-
-    public String content()
-    {
-        return this.name;
-    }
-
-    public Stream<String> keywords()
-    {
-        return null;
     }
 }
